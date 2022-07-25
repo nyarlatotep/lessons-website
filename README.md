93334248ed92b6cc4cc972e7eea0f63d40f83d73
nyarlatotep <danielcrespo@cresalex.com>
2022-07-25 01:58:47 +0000

    Deploy app to GitHub Pages

--- README.md
+++
@@ -1,7 +0,0 @@
-# Learning Lessons Simple Page Application
-
-## Description:  
-
-A simple web page for my niece, in which she will can practice, learn and study English lessons. This SPA uses React.js
-
----
--- src/chatroom/data/chatRooms.js
+++
@@ -1,8 +0,0 @@
-const chatRooms = [
-	{ id: 'dogs', title: '🐶 Dogs 🐶' },
-	{ id: 'food', title: '🍔 Food 🍔' },
-	{ id: 'general', title: '💬 General 💬' },
-	{ id: 'music', title: '🎹 Music 🎹' },
-];
-
-export { chatRooms };
